11a7efd82e18cd7a01dcc3e3a8717a2a9a088adb
Lin <fromlin@snu.ac.kr>
2023-08-03 21:24:30 +0900

    Update class_example.cpp

--- class_example.cpp
+++ class_example.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "example_node");
     ros::NodeHandle nh;
 
-    example ex(&nh);
+    EXAMPLE ex(&nh);
 
     ros::Rate loop_rate(100);
     while (ros::ok())
@@ -15,4 +15,18 @@ int main(int argc, char **argv)
         loop_rate.sleep();
     }
     return 0;
+}
+
+
+
+void EXAMPLE::init()
+{
+  _pub = _nh.advertise<std_msgs::Float32>("/topic/pub", 1, true);
+  _sub = _nh.subscribe("/topic/sub", 1, &example::CallBack, this);
+}
+
+void EXAMPLE::CallBack(const std_msgs::Float32& msg)
+{
+  temp = msg.data;
+  _pub.publish(temp);
 }
